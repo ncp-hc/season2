d5498e6227a887a90e7ac3ee6c29fc9f187e3f70
Great-Stone <hahohh@gmail.com>
2021-06-09 18:26:04 +0900

    fix

--- session01/example/ncp_provision/main.tf
+++ session01/example/ncp_provision/main.tf
@@ -16,22 +16,22 @@ output "image_no" {
     value = data.ncloud_member_server_images.prod.member_server_images.0
 }
 
-// resource "random_id" "id" {
-//   byte_length = 4
-// }
+resource "random_id" "id" {
+  byte_length = 4
+}
 
-// resource "ncloud_login_key" "key" {
-//   key_name = "${var.login_key_name}${random_id.id.hex}"
-// }
+resource "ncloud_login_key" "key" {
+  key_name = "${var.login_key_name}${random_id.id.hex}"
+}
 
-// data "ncloud_root_password" "rootpwd" {
-//   server_instance_no = ncloud_server.server.id
-//   private_key        = ncloud_login_key.key.private_key
-// }
+data "ncloud_root_password" "rootpwd" {
+  server_instance_no = ncloud_server.server.id
+  private_key        = ncloud_login_key.key.private_key
+}
 
-// data "ncloud_port_forwarding_rules" "rules" {
-//   zone = ncloud_server.server.zone
-// }
+data "ncloud_port_forwarding_rules" "rules" {
+  zone = ncloud_server.server.zone
+}
 
 data "ncloud_member_server_images" "prod" {
  #name_regex = data.terraform_remote_state.image_name.outputs.image_name
