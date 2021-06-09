6dc68c8981e676b3b3cc4a41a2fbd0e72c02bc5f
Great-Stone <hahohh@gmail.com>
2021-06-09 14:48:18 +0900

    debug4

--- session01/example/packer/pkr_hcl/variable.pkr.hcl
+++ session01/example/packer/pkr_hcl/variable.pkr.hcl
@@ -1,11 +1,9 @@
 variable "access_key" {
   type    = string
-  default = ""
 }
 
 variable "secret_key" {
   type    = string
-  default = ""
 }
 
 variable "image_name" {
--- session01/example/packer/variable.tf
+++ session01/example/packer/variable.tf
@@ -1,13 +1,3 @@
-variable "access_key" {
-  type    = string
-  default = ""
-}
-
-variable "secret_key" {
-  type    = string
-  default = ""
-}
-
 variable "image_name_prefix" {
   type    = string
   default = "test"
