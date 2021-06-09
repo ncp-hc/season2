134397691748f166a8f5cb3322b769a527d6f184
Great-Stone <hahohh@gmail.com>
2021-06-09 18:25:00 +0900

    fix

--- session01/example/ncp_provision/main.tf
+++ session01/example/ncp_provision/main.tf
@@ -41,35 +41,35 @@ data "ncloud_member_server_images" "prod" {
   }
 }
 
-// resource "ncloud_server" "server" {
-//   name                      = "${var.server_name}${random_id.id.hex}"
-//   member_server_image_no    = data.ncloud_member_server_image.prod.no 
-//   server_product_code       = "SPSVRGPUSSD00001" 
-//   login_key_name            = ncloud_login_key.key.key_name
-//   zone                      = var.zone
-// }
+resource "ncloud_server" "server" {
+  name                      = "${var.server_name}${random_id.id.hex}"
+  member_server_image_no    = data.ncloud_member_server_images.prod.member_server_images.0
+  server_product_code       = "SPSVRGPUSSD00001" 
+  login_key_name            = ncloud_login_key.key.key_name
+  zone                      = var.zone
+}
 
-// #public ip service
-// resource "ncloud_public_ip" "public_ip" {
-//   server_instance_no = ncloud_server.server.id
-// }
+#public ip service
+resource "ncloud_public_ip" "public_ip" {
+  server_instance_no = ncloud_server.server.id
+}
 
-// resource "null_resource" "host_provisioner" {
-//   provisioner "remote-exec" {
-//     inline = [
-//       "touch /tmp/test"
-//     ]
-//   }
+resource "null_resource" "host_provisioner" {
+  provisioner "remote-exec" {
+    inline = [
+      "touch /tmp/test"
+    ]
+  }
 
-//   connection {
-//     type     = "ssh"
-//     host     = ncloud_public_ip.public_ip.public_ip
-//     user     = "root"
-//     port     = "22" 
-//     password = data.ncloud_root_password.rootpwd.root_password
-//   }
-// }
+  connection {
+    type     = "ssh"
+    host     = ncloud_public_ip.public_ip.public_ip
+    user     = "root"
+    port     = "22" 
+    password = data.ncloud_root_password.rootpwd.root_password
+  }
+}
 
-// output "cn_host_pw" {
-//   value = "sshpass -p '${data.ncloud_root_password.rootpwd.root_password}' ssh root@${ncloud_public_ip.public_ip.public_ip} -oStrictHostKeyChecking=no"
-// }
+output "cn_host_pw" {
+  value = "sshpass -p '${data.ncloud_root_password.rootpwd.root_password}' ssh root@${ncloud_public_ip.public_ip.public_ip} -oStrictHostKeyChecking=no"
+}
