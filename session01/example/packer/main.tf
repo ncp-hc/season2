4114cfed3f7f4d5eb5fa2d36d008d735decdc8ef
Great-Stone <hahohh@gmail.com>
2021-06-09 14:44:18 +0900

    debug2

--- session01/example/packer/main.tf
+++ session01/example/packer/main.tf
@@ -12,14 +12,12 @@ resource "null_resource" "run_packer" {
     command = <<EOH
 cd ./pkr_hcl
 unzip ./packer_1.7.2_linux_amd64.zip
-echo $${NCP_ACCESS_KEY}
-echo $${NCP_SECRET_KEY}
 ./packer version
 ./packer init ./ncloud.pkr.hcl
 ./packer build \
     -var 'image_name=${local.last_image_name}' \
-    -var 'access_key=$${NCP_ACCESS_KEY}' \
-    -var 'secret_key=$${NCP_SECRET_KEY}' \
+    -var 'access_key=$$NCP_ACCESS_KEY' \
+    -var 'secret_key=$$NCP_SECRET_KEY' \
     -force \
     -color=false \
     .
