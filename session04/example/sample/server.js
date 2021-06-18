8fd2a91a982598941bee67c57c7ed0e74cb66152
Great-Stone <hahohh@gmail.com>
2021-06-18 14:51:18 +0900

    update

--- session04/example/sample/server.js
+++ session04/example/sample/server.js
@@ -1,44 +1,85 @@
 const http = require('http');
+const Vault = require('hashi-vault-js');
 
-let vaultoption = {
-    apiVersion: "v1",
-    endpoint: `http://101.101.216.84:8200`,
-};
-let VaultSDK = require("node-vault")(vaultoption);
-const username = "admin"
-const password = "password"
+const vault = new Vault( {
+    https: false,
+    baseUrl: `http://101.101.216.84:8200/v1`,
+    rootPath: 'kv',
+    timeout: 2000,
+    proxy: false
+});
 
-VaultSDK.userpassLogin({ username, password })
-.then((result) => {
-    console.log(result)
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');    
-    res.write('<h1>Welcome to NCP X Hashicorp</h1>');
-    res.write('<p>NaverCloud Platform CI/CD X vault !!!</p>');
-    res.end()
-})
-.catch((err) => console.error(err.message));
+let token = ""
+// Start function
+const start = async () => {
+    try {
+        const status = await vault.healthCheck();
+        console.log(status)
+        const login_result = await new Vault( {
+            https: false,
+            baseUrl: `http://101.101.216.84:8200/v1`,
+            timeout: 2000,
+            proxy: false
+        }).loginWithUserpass("admin", "password");
+        console.log(login_result)
+        token = login_result.client_token
+    }
+    catch (err) {
+        if(err.isVaultError) {
+            console.log(err.vaultHelpMessage);
+        }
+        else {
+            throw err;
+        }
+    }
+}
+start()
 
-const server = http.createServer((req, res) => {
-    VaultSDK.read('kv/data/ncp/secret_value')
-    .then((secret_value) => {
-        console.log(secret_value.data.data)
-        secret_number = secret_value.data.data.number
-        VaultSDK.read('kv/data/ncp/mysql_password')
-        .then((mysql_password) => {
-            console.log(mysql_password.data.data)
-            mysql_password = mysql_password.data.data.password
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');    
-            res.write('<h1>Welcome to NCP X Hashicorp</h1>');
-            res.write('<hr>');
-            res.write('<h2>NaverCloud Platform CI/CD X vault !!!</h2>');
-            res.write(`<h2>secret_value is ${secret_number}</h2>`);
-            res.write(`<h2>mysql_password is ${mysql_password}</h2>`);
-            res.end()
-        })
-    })
-    .catch((err) => console.error(err.message));
+const server = http.createServer(async (req, res) => {
+    try {
+        console.log(token)
+        const secret_data = await vault.readKVSecret(token, 'ncp/secret_value');
+        const mysql_password_data = await vault.readKVSecret(token, 'ncp/mysql_password');
+        const static_data = await vault.readKVSecret(token, 'ncp/static_data');
+        secret_number = secret_data.data.number
+        mysql_password = mysql_password_data.data.password
+        body_color = static_data.data.color
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');    
+        res.write(`
+        <html>
+            <head>
+                <title>NCP x HashiCorp</title>
+                <style>
+                h2 {text-align: center; color: white;}
+                h3 {text-align: center; color: white;}
+                h1 {
+                    padding: 60px;
+                    text-align: center;
+                    color: white;
+                    font-size: 60px;
+                }
+                </style>
+            </head>
+            <body style="background-color: ${body_color};">
+                <h1>Welcome to NCP X Hashicorp</h1>
+                <h2>NaverCloud Platform CI/CD X HashiCorp Vault</h2>
+                <hr>
+                <h3>secret_value is ${secret_number}</h3>
+                <h3>mysql_password is ${mysql_password}</h3>
+            </body>
+        </html>
+`);
+        res.end()
+    }
+    catch (err) {
+        if(err.isVaultError) {
+            console.log(err.vaultHelpMessage);
+        }
+        else {
+            throw err;
+        }
+    }
 });
 
 server.listen(3000, '0.0.0.0', () => {
